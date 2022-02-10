6fb04ee2ab840aa7efd1849db2cc5163ba12a358
Felipe <antoniofelipesilva@live.com>
2022-02-10 12:18:03 -0300

    versao_4

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mostrarPopup() {
+  window.alert('Hello World')
+}
+let email = document.getElementById('campo-email')
+
+function enviarEmail() {
+  let emailDigitado = email.value
+  console.log(emailDigitado)
+}
